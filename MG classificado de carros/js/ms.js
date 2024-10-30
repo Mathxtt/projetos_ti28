f0637e98536fa4568f8458b96a7c6cc521037d61
Mathxt <matheusgabgab@gmail.com>
2024-10-30 10:56:30 -0300

    -= MGVeículos =-
    Terminei o slide e o catalogo de veículos

--- MG classificado de carros/js/ms.js
+++ MG classificado de carros/js/ms.js
@@ -1,6 +1,40 @@
+// INICIO MENU DO SITE
 let menu = document.querySelector(".menuLinks");
 let menuDois = document.querySelector("#menuBtn");
 
-menuDois.onclick = () =>{
+menuDois.onclick = () => {
     menu.classList.toggle('active');
 }
+//  FIM MENU DO SITE
+
+// scroll
+Window.onscroll = () =>{
+    let topoSite = document.querySelector("header");
+
+    topoSite.classList.toggle('sticky', window.scrollY > 100);
+}
+
+// INICIO CONTADOR DE VEICULOS 
+
+let campoConta = document.querySelectorAll('.numero');
+
+let intervalo = 1000;
+
+campoConta.forEach((campoConta) => {
+
+    let valorInicial = 0;
+
+    let finalValor = parseInt(campoConta.getAttribute("mg-c-valor"));
+
+    let duracao = Math.floor(intervalo/finalValor);
+
+    let contador = setInterval(function (){
+        valorInicial += 1;
+
+        campoConta.textContent = valorInicial;
+
+        if(valorInicial == finalValor){
+            clearInterval(contador);
+        }
+    }, duracao)
+})
